2a0916fdc119cbe0b0a69aa72e76acc78e279e1d
Anthony Li <llja0112@gmail.com>
2018-12-01 07:42:11 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+
+# from datetime import datetime
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    # the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+    # return app.send_static_file('index.html')
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    # app.run(debug=True, use_reloader=True)
+    app.run()
